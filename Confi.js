9b212b439efca82d4df02314542b8163dcfd8fb8
Diogo Silva Dorta <101647390+diogoodorta@users.noreply.github.com>
2022-06-20 10:33:36 -0300

    Add files via upload
    continuação do projeto de DJW.

---
+++ Confi.js
@@ -0,0 +1,54 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// gera um número aleatório
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// gera uma cor aleatória
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+
+//define um vetor de bolas
+const bolas = [];
+
+while (bolas.length < 25) {
+   const size = random(15,30);
+   const bola = new Bola(
+      // posição de sempre uma bola de distância
+      // fora das bordas para evitar erros de desenho
+      random(0 + size,width - size),
+      random(0 + size,height - size),
+      random(10,15),
+      random(10,15),
+      randomRGB(),
+      size
+   );
+
+   //atualiza o vetor
+  bolas.push(bola);
+}
+
+//realiza um loop em todas as bolas geradas
+function loop() {
+   ctx.fillStyle = 'rgba(0, 0, 0, 0.)'; 
+   ctx.fillRect(0, 0,  width, height);
+
+   for (const bola of bolas) {
+    bola.draw();
+    bola.update();
+    bola.collisionDetect(bolas);
+   }
+
+   requestAnimationFrame(loop);
+}
+
+loop();
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Projeto-DJW-
 Elaborar um projeto HTML/JS baseado no exemplo de orientação a objeto explicado em aula, contento pelo menos 1 classe e 3 atributos diferentes.
+
+13/06/2022: Iniciamos o projeto, criando a pasta do mesmo e colocando os códigos iniciais.
+
+20/06/2022: Continuamos com o projeto, colocando os códigos da classe "Bola" e configurando a mesma. 
